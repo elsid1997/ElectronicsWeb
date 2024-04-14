fcefaa96fc6ce8c6de4878f694c1dbe18f889c2d
elsid1997 <kronusanarx@gmail.com>
2024-04-14 16:03:39 +0400

    dobovliaem izmeneniya v changeDelete

--- administration/static/js/changeDelete.js
+++ administration/static/js/changeDelete.js
@@ -1,12 +1,52 @@
 document.addEventListener('DOMContentLoaded', function(){
     console.log('changeDelete is working')
-    let changeUserAdmin = document.getElementsByClassName('changeAdmin')
-    console.log(changeUserAdmin)
+    let changeUserAdmin = document.getElementsByClassName('changeUser')
     for(let change of changeUserAdmin){
         change.addEventListener('click', function(){
             console.log('change is working')
-            let  adminTd = this.previousSibling
-            adminTd.textContent = ''
+            let  userData = this.parentNode;
+            let fragment = document.createDocumentFragment();
+            for(let data  of userData.children){
+                data.style.padding = '0'
+                if(data.children[0] == undefined){
+                    let input = document.createElement('input');
+                    input.type = 'text';
+                    input.style.backgroundColor = 'gray';
+                    input.style.color = '#fff';
+                    input.style.border = 'none';
+                    input.style.fontSize = '20px';
+                    input.style.width = '100%';
+                    input.style.height = '100%';
+                    input.style.paddingLeft = '5px';
+                    input.style.paddingRight = '5px';
+                    input.value = data.textContent;
+                    data.textContent = '';
+                    fragment.appendChild(input);
+                }else if(data.children[0].tagName == 'IMG'){
+                    let btn = document.createElement('button')
+                    if(data.className == 'changeUser'){
+                        btn.style.backgroundColor = 'rgb(14,89,187)';
+                        btn.textContent = 'ok';
+                        btn.id = 'change-user'
+                    }else {
+                        btn.style.backgroundColor = 'red'
+                        btn.innerText = 'X';
+                        btn.id = 'cancel-user'
+                    }
+                    data.style.width = '40px';
+                    data.style.height = '30px';
+                    btn.style.width = '100%';
+                    btn.style.height = '100%';
+                    btn.style.fontSize = '25px';
+                    btn.style.color = '#fff';
+                    btn.style.border = 'none'
+                    data.innerHTML = '';
+                    fragment.appendChild(btn);
+                }
+            }
+
+            userData.appendChild(fragment.childNodes);
+
         })
     }
 })
--- administration/static/js/pagination.js
+++ administration/static/js/pagination.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded',function(){
             }
 
             const changeCell = document.createElement('td');
-            changeCell.classList.add('changeAdmin');
+            changeCell.classList.add('changeUser');
             const imgChange = document.createElement('img');
             imgChange.src = 'static/img/icons/change.png';
             imgChange.alt = 'change';
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded',function(){
             newRow.appendChild(changeCell);
 
             const deleteCell = document.createElement('td');
-            deleteCell.classList.add('deleteAdmin');
+            deleteCell.classList.add('deleteUser');
             const imgDelete = document.createElement('img');
             imgDelete.src = 'static/img/icons/delete.png';
             imgDelete.alt = 'delete';
@@ -174,154 +174,3 @@ document.addEventListener('DOMContentLoaded',function(){
     window.createTableData = createTableData;
 })
 
-//document.addEventListener('DOMContentLoaded', function (){
-//    console.log('pagination.js is working')
-//
-//     function changePage() {
-//        console.log('changePage is working');
-//
-//        let tableBody = document.querySelector('tbody');
-//        tableBody.innerHTML = '';
-//
-//        let users = JSON.parse(localStorage.getItem('userData');
-//        let currentPage = Number(this.textContent);
-//        let backPage = currentPage - 1;
-//        let nextPage = currentPage + 1;
-//
-//        if (currentPage !== '<' && currentPage !== '>'){
-//
-//            let a = Number(this.textContent)
-//            let b = Number(this.previousSibling.textContent)
-//
-//            nextPage.id = a+1
-//            backPage.id = a-1
-//            console.log(this.textContent,this.previousSibling.textContent)
-//
-//            if(a > b){
-//                  createRows(tableBody,users.slice(b*10,a*10))
-//                  console.log(users.slice(b*10,a*100))
-//                  console.log(users)
-//            }else if(a===1){
-//                createRows(tableBody,users.slice(0,a*10))
-//            }else{
-//                createRows(tableBody,users.slice(b/10,a/10))
-//            }
-//        }else if(this.textContent === '<' && !this.id || this.id == '0'){
-//            nextPage.id = 2
-//            backPage.id = 0
-//            createRows(tableBody,users.slice(0,10))
-//        }else if(this.textContent === '<' && this.id){
-//
-//            let back = Number(this.id)
-//            nextPage.id = back + 1
-//            backPage.id = back - 1
-//
-//            back *= 10
-//            createRows(tableBody,users.slice(back-10,back))
-//        }else if(this.textContent === '>' && !this.id){
-//            nextPage.id= 3
-//            backPage.id = 1
-//            createRows(tableBody,users.slice(10,20))
-//        }else if(this.textContent === '>' && this.id && users.slice((Number(backPage.id)+1)*10, (Number(nextPage.id))*10)[0]){
-//            console.log(users.slice((Number(backPage.id)+1)*10, (Number(nextPage.id))*10)[0])
-//            let next = Number(this.id)
-//            nextPage.id = next + 1
-//            backPage.id = next - 1
-//            next*=10
-//            createRows(tableBody,users.slice(next-10,next))
-//        }else if(!users.slice((Number(backPage.id)+1)*10, (Number(nextPage.id))*10)[0]){
-//            createRows(tableBody, users.slice((Number(backPage.id))*10, (Number(nextPage.id)-1)*10))
-//        }
-//    };
-//    function createRows(tableBody,res){
-//
-//        console.log('createRows is working')
-//
-//        if(res){
-//            for(i=0;i< res.length;i++){
-//                let newRow = tableBody.insertRow();
-//
-//                let cel1 = newRow.insertCell(-1);
-//                cel1.textContent = res[i].name
-//
-//                let cel2 = newRow.insertCell(-1);
-//                cel2.textContent = res[i].surname
-//
-//                let cel3 = newRow.insertCell(-1);
-//                cel3.textContent = res[i].email
-//
-//                let cel4 = newRow.insertCell(-1);
-//                cel4.textContent = res[i].admin
-//
-//                let cel5 = newRow.insertCell(-1);
-//                let imgChange = document.createElement('img');
-//                imgChange.src = "static/img/icons/change.png";
-//                imgChange.alt = 'change';
-//                imgChange.classList.add('change');
-//                cel5.appendChild(imgChange);
-//
-//                let cel6 = newRow.insertCell(-1);
-//                let imgDelete = document.createElement('img');
-//                imgDelete.src = "static/img/icons/delete.png";
-//                imgDelete.alt = 'delete';
-//                imgDelete.classList.add('delete')
-//                cel6.appendChild(imgDelete)
-//            }
-//        }
-//    }
-//    function createPagination(totalRecords, recordsPerPage = 10){
-//
-//        console.log('createPagination is working')
-//
-//        const totalPages = Math.ceil(totalRecords / recordsPerPage);
-//        const pagination = document.getElementById('pagination')
-//        pagination.innerHTML = '';
-//
-//        const prevButton = document.createElement('a');
-//        prevButton.href = '#';
-//        prevButton.textContent = '<';
-//        prevButton.classList.add('back-page');
-//        prevButton.addEventListener('click',changePage)
-//        pagination.appendChild(prevButton);
-//
-//        for(let i=1; i < totalPages+1; i++){
-//            const pageButton = document.createElement('a');
-//            pageButton.href = '#';
-//            pageButton.textContent = i;
-//            pageButton.addEventListener('click',changePage)
-//            pagination.appendChild(pageButton);
-//        };
-//
-//        const nextButton = document.createElement('a');
-//        nextButton.href = '#';
-//        nextButton.textContent = '>';
-//        nextButton.classList.add('next-page');
-//        nextButton.addEventListener('click',changePage)
-//        pagination.appendChild(nextButton);
-//    }
-//
-//    function createTableData(){
-//
-//        console.log('createTableRows is working')
-//
-//        let userDataString = localStorage.getItem('userData')
-//
-//        if(userDataString) {
-//
-//            let res = JSON.parse(userDataString)
-//
-//            let tableBody = document.querySelector('tbody')
-//
-//            if(!tableBody.innerHTML.trim()) {
-//                    createRows(tableBody,res.slice(0,10))
-//                }
-//
-//                const totalRecords = res.length;
-//
-//                createPagination(totalRecords)
-//            }
-//
-//        }
-//
-//    window.createTableData = createTableData
-//})
