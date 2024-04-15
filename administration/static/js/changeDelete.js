b28bdf9d1622e6d1c6621e5c9300758788b4391d
elsid1997 <kronusanarx@gmail.com>
2024-04-15 15:32:12 +0400

    rabotaem nad changeDelete.js

--- administration/static/js/changeDelete.js
+++ administration/static/js/changeDelete.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function(){
         change.addEventListener('click', function(){
             console.log('change is working')
             let  userData = this.parentNode;
-            let fragment = document.createDocumentFragment();
             for(let data  of userData.children){
                 data.style.padding = '0'
                 if(data.children[0] == undefined){
@@ -21,31 +20,40 @@ document.addEventListener('DOMContentLoaded', function(){
                     input.style.paddingRight = '5px';
                     input.value = data.textContent;
                     data.textContent = '';
-                    fragment.appendChild(input);
+                    data.appendChild(input);
                 }else if(data.children[0].tagName == 'IMG'){
                     let btn = document.createElement('button')
                     if(data.className == 'changeUser'){
                         btn.style.backgroundColor = 'rgb(14,89,187)';
                         btn.textContent = 'ok';
+                        btn.style.fontSize = '25px';
                         btn.id = 'change-user'
                     }else {
-                        btn.style.backgroundColor = 'red'
-                        btn.innerText = 'X';
-                        btn.id = 'cancel-user'
+                        let beforeLine = document.createElement('div');
+                        let afterLine = document.createElement('div');
+                        let lineStyle = 'position: absolute; width: 3px; height: 25px; background-color: #fff; top: 50%; left: 50%; transform-origin: center;';
+                        beforeLine.style.cssText = lineStyle + 'transform: translate(-50%,-50%) rotate(45deg);';
+                        afterLine.style.cssText = lineStyle + 'transform: translate(-50%, -50%) rotate(-45deg);';
+                        btn.style.backgroundColor = 'red';
+                        btn.style.position = 'relative';
+                        btn.id = 'cancel-user';
+                        btn.appendChild(beforeLine);
+                        btn.appendChild(afterLine);
                     }
+                    data.style.position = 'relative';
                     data.style.width = '40px';
                     data.style.height = '30px';
+                    btn.style.position = 'absolute';
+                    btn.style.top = '0';
+                    btn.style.left = '0';
                     btn.style.width = '100%';
                     btn.style.height = '100%';
-                    btn.style.fontSize = '25px';
                     btn.style.color = '#fff';
                     btn.style.border = 'none'
                     data.innerHTML = '';
-                    fragment.appendChild(btn);
+                    data.appendChild(btn);
                 }
             }
-
-            userData.appendChild(fragment.childNodes);
 
         })
     }
