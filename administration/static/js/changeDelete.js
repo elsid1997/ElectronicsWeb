9dbd3755e2230996fe596f5bfbd368096b185fbe
elsid1997 <kronusanarx@gmail.com>
2024-04-23 16:12:51 +0400

    23.04.24

--- administration/static/js/changeDelete.js
+++ administration/static/js/changeDelete.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
     console.log('changeDelete is working')
-
     function changeDataUser(){
             console.log('change is working')
-            let  userData = this.parentNode;
+            let userData = this.parentNode;
+            if (!changeDataUser.initialUserData) {
+                changeDataUser.initialUserData = this.parentNode.cloneNode(true);
+            }
             for(let data  of userData.children){
                 data.style.padding = '0'
                 console.log(data.textContent)
@@ -20,10 +22,18 @@ document.addEventListener('DOMContentLoaded', function(){
                         input.style.marginLeft = '5px';
                         input.checked = yesORno == value;
                         input.style.transform = 'scale(1.3)';
+                        input.id = !document.getElementById(value) ? value : value + 1;
+                        console.log(!document.getElementById(value),input.id)
+                        input.id = value;
+                        input.addEventListener('change', function(){
+                            let secondInput = this.id == 'Yes' ? document.getElementById('No') : document.getElementById('Yes');
+                            secondInput.checked = false;
+                        })
 
                         let label = document.createElement('label');
                         label.textContent = value;
                         if (value == 'No') label.style.marginLeft = '10px';
+                        label.setAttribute('for', value);
 
                         data.appendChild(label);
                         data.appendChild(input);
@@ -39,6 +49,7 @@ document.addEventListener('DOMContentLoaded', function(){
                     input.style.height = '100%';
                     input.style.paddingLeft = '5px';
                     input.style.paddingRight = '5px';
+                    input.style.boxSizing = 'border-box';
                     input.value = data.textContent;
                     data.textContent = '';
                     data.appendChild(input);
@@ -48,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function(){
                         btn.style.backgroundColor = 'rgb(14,89,187)';
                         btn.textContent = 'ok';
                         btn.style.fontSize = '25px';
-                        btn.id = 'change-user'
+                        btn.className = 'change-data'
                     }else {
                         let beforeLine = document.createElement('div');
                         let afterLine = document.createElement('div');
@@ -57,9 +68,20 @@ document.addEventListener('DOMContentLoaded', function(){
                         afterLine.style.cssText = lineStyle + 'transform: translate(-50%, -50%) rotate(-45deg);';
                         btn.style.backgroundColor = 'red';
                         btn.style.position = 'relative';
-                        btn.id = 'cancel-user';
+                        btn.className = 'cancel-change';
                         btn.appendChild(beforeLine);
                         btn.appendChild(afterLine);
+                        btn.addEventListener('click', function () {
+                            userData.innerHTML = '';
+                            let oldUserData = changeDataUser.initialUserData.children
+                            Object.keys(oldUserData).forEach(key => {
+                                  let item = oldUserData[key].cloneNode(true)
+                                  if (item.className == 'changeUser'){
+                                    item.addEventListener('click', changeDataUser)
+                                  }
+                                  userData.appendChild(item)
+                            })
+                        });
                     }
                     data.style.position = 'relative';
                     data.style.width = '40px';
